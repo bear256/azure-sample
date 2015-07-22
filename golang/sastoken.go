3a5fa3f140488b6ab34b9829c502bf0253a42047
Panda <bear256@qq.com>
2015-07-22 09:52:49 +0800

    fix bugs

--- golang/sastoken.go
+++ golang/sastoken.go
@@ -4,8 +4,8 @@ import (
 	"crypto/hmac"
 	"crypto/sha256"
 	"encoding/base64"
+	"fmt"
 	"net/url"
-	"time"
 )
 
 const (
@@ -18,7 +18,7 @@ func URLEncode(plain string) string {
 	return values.Encode()[4:]
 }
 
-func HmacSHA256(plain, key string) string {
+func HmacSHA256(str2sig, key string) []byte {
 	mac := hmac.New(sha256.New, []byte(key))
 	mac.Write([]byte(str2sig))
 	hmacsha256 := mac.Sum(nil)
@@ -29,7 +29,7 @@ func GenerateSASToken(expiry, keyName, uri, key string) string {
 	sr := URLEncode(uri)
 	str2sig := sr + "\n" + expiry
 	hmacsha256 := HmacSHA256(str2sig, key)
-	sig := urlencode(base64.StdEncoding.EncodeToString([]byte(hmacsha256)))
+	sig := URLEncode(base64.StdEncoding.EncodeToString(hmacsha256))
 	sasToken := fmt.Sprintf(SAS_TOKEN_TEMP, sig, expiry, keyName, sr)
 	return sasToken
 }
